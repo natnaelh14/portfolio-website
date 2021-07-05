9441befbe338005c0709d60535c136fa25527ed5
Natnael Haile <natnaelhaile@Natnaels-MacBook-Pro.local>
2021-07-04 23:57:58 -0400

    updated nav bar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,51 @@
-import React from 'react';
-import './header.css';
+import React, { useState } from "react";
+import "./header.css";
+import { HiMenuAlt3, HiOutlineX } from "react-icons/hi";
 
 const Header = () => {
-    return (
-      <div className='navbar-main'>
-        <img className="navbar-logo" src='./logo.png' alt=""/>
-        <ul className='navbar-list'>
-          <li><a href='#about'>About</a></li>
-          <li><a href='#projects'>Projects</a></li>
-          <li><a href='#resume'>Resume</a></li>
-          <li><a href='#contact'>Contact</a></li>
-        </ul>
-      </div>
-    )
-}
+  const [showNav, SetshowNav] = useState(false);
+  return (
+    <div className="navbar-main">
+      <img className="navbar-logo" src="./logo.png" alt="" />
+      <ul className="navbar-list">
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#resume">Resume</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <span
+        className="navbar-mobile-icon"
+        onClick={() => SetshowNav((prev) => !prev)}
+      >
+        {showNav ? <HiOutlineX /> : <HiMenuAlt3 />}
+      </span>
+      <ul
+        className={
+          showNav
+            ? "navbar-list--mobile navbar-list-mobileShow"
+            : "navbar-list-mobile"
+        }
+      >
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Header;
