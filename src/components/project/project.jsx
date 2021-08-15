5e1153baed06248c3d021ae5f5e30a17ea170a74
natnaelh14 <natnaelh14@github.com>
2021-08-15 04:34:57 -0400

    added text to project section

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -13,7 +13,7 @@ const Project = ({ title, desc, langs, liveLink, githubLink, imageLink }) => {
       <br/>
       <div className="project-langs">
         {langs.map(lang => (
-        <span className="project-lang"><Icon icon={lang} /></span>
+        <span className="project-lang"><Icon icon={lang} size = '30px' /></span>
         ))}
       </div>
       <div className="project-links">
