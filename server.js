bfa976daf3f7359cdb51a7edabc2b42251d22252
natnaelh14 <natnaelh14@github.com>
2021-08-17 16:54:35 -0400

    created server.js for contact form

---
+++ server.js
@@ -0,0 +1,57 @@
+//https://medium.com/hackernoon/setting-up-a-serverless-contact-form-in-react-using-nodemailer-and-express-19df9fdb20ed
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+const app = express();
+
+const port = 4444;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log('We are live on port 4444');
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my api');
+})
+
+app.post('/api/v1', (req,res) => {
+  var data = req.body;
+
+var smtpTransport = nodemailer.createTransport({
+  service: 'Gmail',
+  port: 465,
+  auth: {
+    user: 'USERNAME',
+    pass: 'PASSWORD'
+  }
+});
+
+var mailOptions = {
+  from: data.email,
+  to: 'ENTER_YOUR_EMAIL',
+  subject: 'ENTER_YOUR_SUBJECT',
+  html: `<p>${data.name}</p>
+          <p>${data.email}</p>
+          <p>${data.message}</p>`
+};
+
+smtpTransport.sendMail(mailOptions,
+(error, response) => {
+  if(error) {
+    res.send(error)
+  }else {
+    res.send('Success')
+  }
+  smtpTransport.close();
+});
+
+})
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,30 +1,48 @@
-import React from "react";
-import { useForm, FormProvider } from "react-hook-form";
+import { React, Component } from "react";
 import { Fade } from "react-reveal";
-import Test from "./Test";
 import "./contact.css";
 
-const Contact = () => {
-  const methods = useForm();
-  const { register, handleSubmit } = methods;
-  return (
-    <FormProvider {...methods}>
-      <h1 className="contact-heading" id="contact">
-        Contact
-      </h1>
-      <Fade bottom duration={1000} distance="40px">
-        <form onSubmit={handleSubmit((data) => console.log(data))}>
-          <label>Full Name</label>
-          <input {...register("test", { required: true })} id="full-name" name="full-name" type="text" />
-          <label>E-mail Address</label>
-          <input {...register("test", { required: true })} id="email" name="email" type="text" />
-          <label>Message</label>
-          <textarea {...register("test", { required: true })} id="message" name="message" type="message" />
-          <button type="submit">SUBMIT</button>
-        </form>
-      </Fade>
-    </FormProvider>
-  );
-};
+export default class Contact extends Component {
+  state = {
+    name: "",
+    message: "",
+    email: "",
+    sent: false,
+  };
 
-export default Contact;
+  render() {
+    return (
+      <div>
+        <h1 className="contact-heading" id="contact">
+          Contact
+        </h1>
+        <Fade bottom duration={1000} distance="40px">
+          <form onSubmit={(e) => this.formSubmit(e)}>
+            <label>Full Name</label>
+            <input
+              onChange={(e) => this.setState({ name: e.target.value })}
+              id="full-name"
+              name="full-name"
+              type="text"
+            />
+            <label>E-mail Address</label>
+            <input
+              onChange={(e) => this.setState({ name: e.target.value })}
+              id="email"
+              name="email"
+              type="text"
+            />
+            <label>Message</label>
+            <textarea
+              onChange={(e) => this.setState({ name: e.target.value })}
+              id="message"
+              name="message"
+              type="message"
+            />
+            <button type="submit">SUBMIT</button>
+          </form>
+        </Fade>
+      </div>
+    );
+  }
+}
