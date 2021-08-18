0aeb5ae68682798bef006e58a12174ce60b28d76
natnaelh14 <natnaelh14@github.com>
2021-08-18 15:29:40 -0400

    created conditionals and error handling for contact form

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,55 +1,87 @@
 import { React, Component } from "react";
 import { Fade } from "react-reveal";
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 import "./contact.css";
 
 export default class Contact extends Component {
   state = {
     name: "",
     email: "",
-    subject: '',
+    subject: "",
     message: "",
     sent: false,
-    buttonText: "Submit",
+    buttonText: "complete form",
+    errorText: "",
   };
 
   formSubmit = (e) => {
     e.preventDefault();
-    this.setState({
-      buttonText: "...sending",
-    });
+    if (
+      this.state.name &&
+      this.state.email &&
+      this.state.subject &&
+      this.state.message
+    ) {
+      this.setState({
+        name: this.state.name,
+        message: this.state.message,
+        subject: this.state.subject,
+        email: this.state.email,
+        buttonText: "...sending",
+      });
 
-    let templateParams = {
-      from_name: this.state.name,
-      from_email: this.state.email,
-      to_name: 'Natnael Haile',
-      subject: this.state.subject,
-      message: this.state.message,
-     }
+      let templateParams = {
+        from_name: this.state.name,
+        from_email: this.state.email,
+        to_name: "Natnael Haile",
+        subject: this.state.subject,
+        message: this.state.message,
+      };
 
-    emailjs.send(
-      'service_nasoi6g',
-      'template_18f6zsf',
-       templateParams,
-      'user_eZV3e0rLSAkwzx3Pvay2V'
-     )
-    console.log(templateParams)
-    this.resetForm()
+      emailjs.send(
+        "service_nasoi6g",
+        "template_18f6zsf",
+        templateParams,
+        "user_eZV3e0rLSAkwzx3Pvay2V"
+      );
+      console.log(templateParams);
+      this.resetForm();
+    } else {
+      this.setState({
+        errorText: "Please complete the required fields",
+      });
+      setTimeout(() => {
+        this.setState({ errorText: "" });
+    }, 2000);
+    }
   };
 
   resetForm = () => {
     this.setState({
       name: "",
       message: "",
-      subject: '',
+      subject: "",
       email: "",
       buttonText: "Message Sent",
     });
+    setTimeout(() => {
+      this.setState({ buttonText: "complete form" });
+  }, 4000);
   };
 
   handleChange = (param, e) => {
-    this.setState({ [param]: e.target.value })
-  }
+    this.setState({ [param]: e.target.value });
+    if (
+      this.state.name &&
+      this.state.email &&
+      this.state.subject &&
+      this.state.message
+    ) {
+      this.setState({
+        buttonText: "submit",
+      });
+  };
+}
 
   render() {
     return (
@@ -61,7 +93,7 @@ export default class Contact extends Component {
           <form onSubmit={(e) => this.formSubmit(e)}>
             <label>Full Name</label>
             <input
-              onChange={this.handleChange.bind(this, 'name')}
+              onChange={this.handleChange.bind(this, "name")}
               value={this.state.name}
               autoComplete="off"
               id="full-name"
@@ -70,7 +102,7 @@ export default class Contact extends Component {
             />
             <label>E-mail Address</label>
             <input
-              onChange={this.handleChange.bind(this, 'email')}
+              onChange={this.handleChange.bind(this, "email")}
               value={this.state.email}
               autoComplete="off"
               id="email"
@@ -79,7 +111,7 @@ export default class Contact extends Component {
             />
             <label>Subject</label>
             <input
-              onChange={this.handleChange.bind(this, 'subject')}
+              onChange={this.handleChange.bind(this, "subject")}
               value={this.state.subject}
               autoComplete="off"
               id="subject"
@@ -88,13 +120,14 @@ export default class Contact extends Component {
             />
             <label>Message</label>
             <textarea
-              onChange={this.handleChange.bind(this, 'message')}
+              onChange={this.handleChange.bind(this, "message")}
               value={this.state.message}
               autoComplete="off"
               id="message"
               name="message"
               type="message"
             />
+            <p style={{color: '#e64861', textAlign: 'center'}}>{this.state.errorText}</p>
             <button type="submit">{this.state.buttonText}</button>
           </form>
         </Fade>
