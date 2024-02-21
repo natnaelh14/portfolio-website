bd093dd3d4b83102933782ca02277774e88f2409
natnaelh14 <natnaelh14@github.com>
2024-02-20 21:39:20 -0500

    store tech lists in a constants file.

---
+++ src/constants.ts
@@ -0,0 +1,37 @@
+export const frontEndTech = [
+	"HTML5",
+	"CSS3",
+	"Tailwind",
+	"Styled Components",
+	"JavaScript",
+	"jQuery",
+	"React",
+	"Redux",
+	"Next.js",
+	"TypeScript",
+] as const;
+
+export const backEndTech = [
+	"Node/Express.js",
+	"Java",
+	"Spring Boot",
+	"Prisma",
+	"MySQL",
+	"PostgreSQL",
+	"MongoDB",
+	"GraphQL/Apollo",
+	"Postman",
+] as const;
+
+export const otherTech = [
+	"Git/Terminal",
+	"Agile Development",
+	"Algorithms/Data Structures",
+	"CI/CD",
+	"GitHub Actions",
+	"Google Analytics",
+	"Contentful",
+	"Jest Testing",
+	"Docker",
+	"AWS",
+] as const;
